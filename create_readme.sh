7164c127e6631397e27a88a8cac72fb5253edb24
curlly <toto-du-91100@hotmail.fr>
2015-10-21 16:41:05 +0200

    seconde modif de readme md

--- create_readme.sh
+++ create_readme.sh
@@ -2,7 +2,7 @@ rm README.md
 echo "#PROJET --JENA--" >> README.md
 echo "###DEVELOPEMENT OPEN SOURCE" >> README.md
 echo "ECOLE PREP ETNA ALTERNANCE, FRANCE" >> README.md
-echo "###DEVELOPEURS" >> README.md
+echo "###DEVELOPPEURS" >> README.md
 echo "- ROLLAND THOMAS (ETNA)" >> README.md
 echo "- BEDMINSTER JOSEPH (ETNA)" >> README.md
 echo "###COLLABORATEURS" >> README.md
@@ -10,9 +10,8 @@ echo "- AZIS WINDA (ETNA)" >> README.md
 echo "- COLAS GEOFFREY (ETNA)" >> README.md
 echo "- KAUCH JONATHAN (EPITECH)" >> README.md
 echo "- FRAYSSE CHRISTOPHE (EPITECH)" >> README.md
-echo "___________________________" >> README.md
+echo "###INFOS" >> README.md
 date >> README.md
-echo "___________________________" >> README.md
 echo "Nombre de lignes : " >> README.md
 cat *.c *.h *.txt | wc -l >> README.md
 echo "Nombre de caracteres : " >> README.md
