eeadee0dc2c8abe34d9835099b99dfb363437ee5
nextjoey <nextjoey@gmail.com>
2015-10-21 18:38:10 +0200

    Delete gameover.c

--- gameover.c
+++
@@ -1,9 +0,0 @@
-#include "fonctions.h"
-#include <stdio.h>
-void		gameover()
-{
-  system("cat skull.txt");
-  my_putstr("\n\n                       ===========================\n\n");
-  my_putstr("                                 GAME OVER\n");
-  my_putstr("\n                       ===========================\n\n");
-}
