08471c01dc274d1dfe6d1a379a0b97017b83bef7
curlly <toto-du-91100@hotmail.fr>
2015-10-21 16:42:26 +0200

    troisieme modif de readme md

--- create_readme.sh
+++ create_readme.sh
@@ -3,13 +3,13 @@ echo "#PROJET --JENA--" >> README.md
 echo "###DEVELOPEMENT OPEN SOURCE" >> README.md
 echo "ECOLE PREP ETNA ALTERNANCE, FRANCE" >> README.md
 echo "###DEVELOPPEURS" >> README.md
-echo "- ROLLAND THOMAS (ETNA)" >> README.md
-echo "- BEDMINSTER JOSEPH (ETNA)" >> README.md
+echo "  ROLLAND THOMAS (ETNA)" >> README.md
+echo "  BEDMINSTER JOSEPH (ETNA)" >> README.md
 echo "###COLLABORATEURS" >> README.md
-echo "- AZIS WINDA (ETNA)" >> README.md
-echo "- COLAS GEOFFREY (ETNA)" >> README.md
-echo "- KAUCH JONATHAN (EPITECH)" >> README.md
-echo "- FRAYSSE CHRISTOPHE (EPITECH)" >> README.md
+echo "  AZIS WINDA (ETNA)" >> README.md
+echo "  COLAS GEOFFREY (ETNA)" >> README.md
+echo "  KAUCH JONATHAN (EPITECH)" >> README.md
+echo "  FRAYSSE CHRISTOPHE (EPITECH)" >> README.md
 echo "###INFOS" >> README.md
 date >> README.md
 echo "Nombre de lignes : " >> README.md
