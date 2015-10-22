eb2df5f17f486b067fa0f0ad193d5adec58d80da
Joseph Bedminster <bedmin_j@etna-alternance.net>
2015-10-22 19:05:47 +0200

    Modif du install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 git clone "https://github.com/nextjoey/jena.git" JENA
-echo "rm ../install.sh" >> jena.sh
+echo "rm install.sh" >> jena.sh
 echo "cd JENA\n" >> jena.sh
 echo "git pull\n" >> jena.sh
 echo "./jena" >> jena.sh
 cd JENA
-sh install.sh
+./jena
