091f013ac5401a9fd4e17a914143e8ec72366b35
Joseph Bedminster <bedmin_j@etna-alternance.net>
2015-10-22 18:48:46 +0200

    MAJ EXEC + jena.sh OK

--- jena.sh
+++ jena.sh
@@ -1,4 +1,6 @@
 git clone "https://github.com/nextjoey/jena.git" JENA
+echo "cd JENA\n" >> update.sh
+echo "git pull\n" >> update.sh
+echo "./jena" >> update.sh
 cd JENA
-echo "git pull\n./jena" >> jena.sh
 ./jena
